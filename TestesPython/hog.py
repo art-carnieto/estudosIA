2672f824d650490d804d7780765d5a19b132d06a
art-carnieto <arthurcarnieto@gmail.com>
2017-04-26 13:53:56 -0300

    Execucao de 7 valores diferentes nos pixels_per_cell que armazena em pastas diferentes, tirei os colchetes ('[' e ']') da saida e corrigi um erro que omitia resultados em saidas com mais de 1000 elementos

--- TestesPython/hog.py
+++ TestesPython/hog.py
@@ -9,11 +9,14 @@ from skimage import color
 from skimage.filters import roberts
 import os
 
+np.set_printoptions(threshold=np.nan) # corrige o tamanho maximo do print na saida do ndarray
+
 # aonde a pasta com as imagens esta
 # os.getcwd ==> pasta atual do arquivo hog.py
 # pode mudar para outra string aonde estejam as imagens
 try:
-	caminhoEntrada = "/home/arthur/SI/IA/EP/dataset1/treinamento"
+	caminhoEntrada = os.getcwd()
+	#caminhoEntrada = "/home/arthur/SI/IA/EP/dataset1/treinamento"
 	arquivosPasta = os.listdir(caminhoEntrada)
 except OSError as err:
 	print("Erro no acesso a pasta com as imagens de entrada: ",err)
@@ -28,41 +31,46 @@ arquivosImagem = list(filter(lambda k: '.png' in k, arquivosPasta))
 # neste site possuem outros algoritmos de deteccao de borda:
 # http://scikit-image.org/docs/dev/auto_examples/edges/plot_edge_filter.html
 
+ppc = [128, 64, 32, 16, 8, 4, 2] # ppc = pixels por celula
+
 if len(arquivosImagem) == 0:
 	print("Pasta selecionada nao contem imagens .png")
 
-for imagem in arquivosImagem:
-	print("Processando imagem " + imagem)
-	try:
-		A = color.rgb2gray(imread(os.path.join(caminhoEntrada, imagem)))
-	except IOError as err:
-		print("Erro na leitura da imagem ", imagem, ": ", err)
-	a1 = roberts(A)
-	v, B = hog(a1,orientations=8, pixels_per_cell=(16, 16),
-		cells_per_block=(1, 1), visualise=True)
+for i in ppc:
+	print("")
+	print("Iniciando HOG com pixels_per_cell = " + str(i) + "x" + str(i))
+	for imagem in arquivosImagem:
+		print("\tProcessando imagem " + imagem)
+		try:
+			A = color.rgb2gray(imread(os.path.join(caminhoEntrada, imagem)))
+		except IOError as err:
+			print("Erro na leitura da imagem ", imagem, ": ", err)
+		a1 = roberts(A)
+		v, B = hog(a1,orientations=8, pixels_per_cell=(i, i),
+			cells_per_block=(1, 1), visualise=True)
 
-	# testes para mostrar as imagens geradas:
-	# imshow(A)
-	# plt.show()
-	# imshow(a1)
-	# plt.show()
-	# imshow(B)
-	# plt.show()
+		# testes para mostrar as imagens geradas:
+		# imshow(A)
+		# plt.show()
+		# imshow(a1)
+		# plt.show()
+		# imshow(B)
+		# plt.show()
 
-	# saida = string imagem menos os 3 ultimos caracteres
-	saida = imagem[:-3]
-	saida = saida + "txt"
-	# print("saida = ",saida)
+		# saida = string imagem menos os 3 ultimos caracteres
+		saida = imagem[:-3]
+		saida = saida + "txt"
+		# print("saida = ",saida)
 
-	try:
-		caminhoSaida = os.path.join(caminhoEntrada,"HOG")
-		if not os.path.exists(caminhoSaida):
-			os.makedirs(caminhoSaida)
-	except OSError as err:
-		print("Erro de acesso a pasta de saida: ", err)
+		try:
+			caminhoSaida = os.path.join(caminhoEntrada,"HOG " + str(i))
+			if not os.path.exists(caminhoSaida):
+				os.makedirs(caminhoSaida)
+		except OSError as err:
+			print("Erro de acesso a pasta de saida: ", err)
 
-	try:
-		f = open(os.path.join(caminhoSaida, saida), 'w')
-		f.write(str(v))
-	except IOError as err:
-		print("Erro na escrita do arquivo ", saida, ": ", err)
+		try:
+			f = open(os.path.join(caminhoSaida, saida), 'w')
+			f.write(str(v)[2:-1])
+		except IOError as err:
+			print("Erro na escrita do arquivo ", saida, ": ", err)
